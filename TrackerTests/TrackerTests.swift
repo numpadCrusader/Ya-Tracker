ef93d15789fdcdbd27f3542cedaddf29b130a9a4
numpadCrusader <103027096+numpadCrusader@users.noreply.github.com>
2025-07-17 09:11:24 +0500

    chore: added new target for snapshot tests

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Nikita Khon on 17.07.2025.
+//
+
+import Testing
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
